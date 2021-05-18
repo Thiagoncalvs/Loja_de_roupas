2a770bc445320aefadead7127fa9bdaf7d052ccf
THIAGO S. GONÇALVES <thiago_gub@hotmail.com>
2021-05-18 12:35:57 -0300

    add1 18mai21

---
+++ JS/funcionalidades.js
@@ -0,0 +1,62 @@
+class SlideStories{
+    constructor(id){
+        this.slide = document.querySelector(`[data-slide="${id}"]`)
+        this.active = 0
+        this.init()
+    }
+    activeSlide(index){
+        this.active = index
+        this.itens.forEach((item) => item.classList.remove('active'))
+        this.itens[index].classList.add('active')
+        this.thumbItens.forEach((item) => item.classList.remove('active'))
+        this.thumbItens[index].classList.add('active')
+        this.autoSlide()
+    }
+
+
+
+    prev (){
+        if(this.active > 0){
+            this.activeSlide(this.active - 1)
+        }else{
+            this.activeSlide(this.itens.length - 1)
+        }
+    }
+
+    next (){
+        if(this.active < this.itens.length - 1){
+            this.activeSlide(this.active + 1)
+        }else{
+            this.activeSlide(0)
+        }
+    }
+
+    addNavigation(){
+        const nextBtn = this.slide.querySelector('.slide-next')
+        const prevBtn = this.slide.querySelector('.slide-prev')
+        nextBtn.addEventListener('click', this.next)
+        prevBtn.addEventListener('click', this.prev)
+    }
+
+    addThumbItens(){
+        this.itens.forEach(() => this.thumb.innerHTML += `<span></span>`)
+        this.thumbItens = Array.from(this.thumb.children)
+    }
+
+    autoSlide(){
+        clearTimeout(this.timeout)
+        this.timeout = setTimeout(this.next, 5000)
+    }
+
+    init(){
+        this.next = this.next.bind(this)
+        this.prev = this.prev.bind(this)
+        this.itens = this.slide.querySelectorAll('.slide-itens > *')
+        this.thumb = this.slide.querySelector('.slide-thumb')
+        this.addThumbItens()
+        this.activeSlide(0)
+        this.addNavigation()
+    }
+}
+
+new SlideStories('slide1')
